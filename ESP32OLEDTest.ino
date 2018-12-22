1204e335ae643b4b645befbf5532eaf729ad099a
Bamamanjam Development <bamamanjam.development@zemljak.si>
2018-12-22 15:47:10 +0100

    BME280 support added
    Replaced "fake" temperature, pressure and humidity values with measured ones. Added gesture controlled display.

--- ESP32OLEDTest.ino
+++ ESP32OLEDTest.ino
@@ -1,5 +1,6 @@
 #include <Wire.h>
 #include <SSD1306.h>
+#include <BME280I2C.h>
 #include <OLEDDisplayUi.h>
 #include <Adafruit_APDS9960.h>
 #include "images.h"
@@ -8,13 +9,26 @@ SSD1306Wire display(0x3C,25,26,GEOMETRY_128_32);
 OLEDDisplayUi ui ( &display );
 Adafruit_APDS9960 apds;
 
+// BME280 sensor settings
+BME280I2C::Settings settings(
+   BME280::OSR_X16,
+   BME280::OSR_X16,
+   BME280::OSR_X16,
+   BME280::Mode_Forced,
+   BME280::StandbyTime_1000ms,
+   BME280::Filter_16,
+   BME280::SpiEnable_False,
+   0x76 // I2C address. I2C specific.
+);
+BME280I2C bme(settings);
+
 unsigned long startTime;
 
 int nFuelLevel;
 bool bBurn;
 int nWiFiLevel;
 
-void drawOverlay1(OLEDDisplay *display, OLEDDisplayUiState* state)
+void drawLeftLogo(OLEDDisplay *display, OLEDDisplayUiState* state)
 {
   if (nFuelLevel<10) display->drawXbm(0, 0, FuelBig_width, FuelBig_height, FuelBig_bits);
   else if (bBurn) display->drawXbm(0, 0, Fire_Logo_width, Fire_Logo_height, Fire_Logo_bits);
@@ -24,46 +38,54 @@ void drawOverlay1(OLEDDisplay *display, OLEDDisplayUiState* state)
   }
 }
 
-void drawOverlay2(OLEDDisplay *display, OLEDDisplayUiState* state)
+void drawFuelLevel(OLEDDisplay *display, OLEDDisplayUiState* state)
 {
   display->drawXbm(50, 0, FuelMini_width, FuelMini_height, FuelMini_bits);
   display->drawProgressBar(60,0,64,8,nFuelLevel);
 }
 
-void drawOverlay3(OLEDDisplay *display, OLEDDisplayUiState* state)
+void drawWiFiLevel(OLEDDisplay *display, OLEDDisplayUiState* state)
 {
   if (nWiFiLevel>70) display->drawXbm(35, 0, WiFi100_width, WiFi100_height, WiFi100_bits);
   else if (nWiFiLevel > 40) display->drawXbm(35, 0, WiFi50_width, WiFi50_height, WiFi50_bits);
   else display->drawXbm(35, 0, WiFi20_width, WiFi20_height, WiFi20_bits);
 }
 
-void drawFrame1(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y)
+void drawTemperatureFrame(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y)
 {
+  String output;
   display->setTextAlignment(TEXT_ALIGN_RIGHT);
   display->setFont(ArialMT_Plain_24);
-  display->drawString(x+127, y+8, "23.7°C");
+  BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
+  output = String(bme.temp(tempUnit),1) + "°C";
+  display->drawString(x+127, y+8, output);
 }
 
-void drawFrame2(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y)
+void drawPressureFrame(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y)
 {
+  String output;
   display->setTextAlignment(TEXT_ALIGN_RIGHT);
   display->setFont(ArialMT_Plain_24);
-  display->drawString(x+127, y+8, "997 hPa");
+  BME280::PresUnit presUnit(BME280::PresUnit_hPa);
+  output = String(bme.pres(presUnit),0) + "hPa";
+  display->drawString(x+127, y+8, output);
 }
 
-void drawFrame3(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y)
+void drawHumidityFrame(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y)
 {
+  String output;
   display->setTextAlignment(TEXT_ALIGN_RIGHT);
   display->setFont(ArialMT_Plain_24);
-  display->drawString(x+127, y+8, "67%");
+  output = String(bme.hum(),0) + "%";
+  display->drawString(x+127, y+8, output);
 }
 
 // Array of frames
-FrameCallback frames[] = { drawFrame1, drawFrame2, drawFrame3 };
+FrameCallback frames[] = { drawTemperatureFrame, drawPressureFrame, drawHumidityFrame };
 int frameCount = 3;
 
 // Array of overlays
-OverlayCallback overlays[] = { drawOverlay1, drawOverlay2, drawOverlay3 };
+OverlayCallback overlays[] = { drawLeftLogo, drawFuelLevel, drawWiFiLevel };
 int overlaysCount = 3;
 
 void setup()
@@ -72,8 +94,8 @@ void setup()
 
   ui.setTargetFPS(30);
   ui.disableAllIndicators();
-  //ui.disableAutoTransition();
-  ui.enableAutoTransition();
+  ui.disableAutoTransition();
+  //ui.enableAutoTransition();
   ui.setFrameAnimation(SLIDE_RIGHT);
   
   // Add frames
@@ -102,6 +124,15 @@ void setup()
     apds.enableGesture(true);
     Serial.println("Device initialized!");
   }
+
+  if(!bme.begin())
+  {
+    Serial.println("Could not find BME280 sensor!");
+  }
+  else
+  {
+    // bmeDataUpdater.attach(UPDATE_INTERVAL_s,bmeUpdateData);
+  }
   
 }
 
@@ -121,12 +152,12 @@ void loop()
     if(gesture == APDS9960_DOWN) 
     {
       Serial.println("v");
-      nFuelLevel-=10;
+      ui.previousFrame();
     }
     if(gesture == APDS9960_UP)
     {
       Serial.println("^");
-      nFuelLevel+=10;
+      ui.nextFrame();
     }
     if(gesture == APDS9960_LEFT) Serial.println("<");
     if(gesture == APDS9960_RIGHT) Serial.println(">");
